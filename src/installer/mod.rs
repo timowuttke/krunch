d72760db2009389824471303b25934f55c676857
Timo Wuttke <timo.wuttke@gmail.com>
2023-04-30 22:59:24 +0200

    copy from tmp to bin works

--- src/installer/mod.rs
+++ src/installer/mod.rs
@@ -6,7 +6,6 @@ use anyhow::{anyhow, Result};
 use std::fs;
 use std::fs::File;
 use std::io::{copy, Cursor};
-use std::ops::Add;
 use std::path::PathBuf;
 use tempfile::Builder;
 
@@ -14,26 +13,36 @@ impl Krunch {
     pub async fn download_all() -> Result<()> {
         let dl = DownloadUrls::new();
 
-        Self::download_file_to_tmp_folder(dl.kubectl).await?;
+        Self::download_file_to_bin_folder(dl.kubectl, "kubcetl").await?;
 
         Ok(())
     }
 
-    async fn download_file_to_tmp_folder(url: String) -> Result<()> {
+    async fn download_file_to_bin_folder(url: String, target_name: &str) -> Result<()> {
         let response = reqwest::get(url).await?;
 
         let tmp_dir = Builder::new().tempdir()?;
-        let file_name = response.url().path_segments().unwrap().last().unwrap();
-        let file_path = tmp_dir.path().join(file_name);
-
-        println!("downloaded to {}", &file_path.to_str().unwrap());
-        let mut file = File::create(file_path)?;
-
-        // let bin_folder = Self::get_bin_folder()?;
-        // fs::create_dir_all(&bin_folder)?;
+        let tmp_file_name = response.url().path_segments().unwrap().last().unwrap();
+        let tmp_file_path = tmp_dir.path().join(tmp_file_name);
 
         let mut content = Cursor::new(response.bytes().await?);
-        copy(&mut content, &mut file)?;
+        let mut tmp_file = File::create(&tmp_file_path)?;
+        copy(&mut content, &mut tmp_file)?;
+
+        Self::handle_tmp_file(tmp_file_path, target_name)?;
+
+        drop(tmp_file);
+        tmp_dir.close()?;
+
+        Ok(())
+    }
+
+    fn handle_tmp_file(tmp_file: PathBuf, target_name: &str) -> Result<()> {
+        let mut bin_folder = Self::get_bin_folder()?;
+        fs::create_dir_all(&bin_folder)?;
+
+        bin_folder.push_str(target_name);
+        fs::copy(tmp_file, bin_folder)?;
 
         Ok(())
     }
