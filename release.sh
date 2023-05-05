401f35349ee94ce31f05fc959803c41a8c0edeaf
Timo Wuttke <timo.wuttke@gmail.com>
2023-05-06 00:18:20 +0200

    basic release script

--- release.sh
+++ release.sh
@@ -1,1 +1,15 @@
-cargo build --target x86_64-pc-windows-gnu
+#!/bin/bash
+
+set -e
+
+VERSION=$(grep -m1 "^version" Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+echo "Building version $VERSION"
+
+#for TARGET in x86_64-pc-windows-gnu x86_64-unknown-linux-gnu x86_64-apple-darwin aarch64-apple-darwin; do
+#    echo "Building target $TARGET"
+#    cargo build --release --target "$TARGET"
+#done
+
+echo "Creating git tag v$VERSION"
+git tag -a "v$VERSION" -m "Release version $VERSION"
+git push origin "v$VERSION"
